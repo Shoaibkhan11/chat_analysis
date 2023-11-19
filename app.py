a7b90eebe3f1cf669f8108b10691fcf72de6415a
Shoaib Khan <suhelsuh11374@yahoo.com>
2023-11-19 15:28:01 +0530

    third commit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import preprocessor
 import helper
 import matplotlib.pyplot as plt
-
+import seaborn as sns
 
 
 st.sidebar.title("WhatsApp chat analysis")
@@ -112,3 +112,16 @@ if uploaded_file is not None:
             ax.bar(busy_day.index,busy_day.values,color='purple')
             plt.xticks(rotation='vertical')
             st.pyplot(fig)
+        with col2:
+            st.header('Most Busy Months')
+            busy_months=helper.month_activity_map(selected_user,df)
+            fig,ax=plt.subplots()
+            ax.bar(busy_months.index,busy_months.values,color='magenta')
+            plt.xticks(rotation='vertical')
+            st.pyplot(fig)
+
+        st.title('Daily Usage Map')
+        activity_df=helper.activity_heatmap(selected_user,df)
+        fig,ax=plt.subplots()
+        ax=sns.heatmap(activity_df)
+        st.pyplot(fig)
